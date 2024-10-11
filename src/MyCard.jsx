a0aba8f6e89f08591f1a5ae3e5ad9e257922e7e5
Earn3011 <earnny3011@gmail.com>
2024-10-11 16:35:21 +0700

    edit bt

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,8 +26,8 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-    <Button size="small">xxx</Button>
-    <Button size="small">xxx</Button>
+    <Button size="small">ADD</Button>
+    <Button size="small">DELETE</Button>
     <Button size="small">Learn More</Button>
   </CardActions>
 </Card>
